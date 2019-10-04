c85b132d7dd414cbfcc45cd6fd3869b71619f25a
Facundo Rotger <facu_rot@hotmail.com>
2019-10-04 15:35:28 -0300

    ready to deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path");
 
 // Connect to Database
 connectDB();
@@ -12,6 +13,16 @@ app.use(express.json({ extended: false }));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/carrozas", require("./routes/api/carrozas"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
